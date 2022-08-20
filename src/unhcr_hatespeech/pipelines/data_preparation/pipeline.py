c2591fe7e80d32b407df73b79326bb96de1504f3
Frederik Gaasdal Jensen <fredefut@hotmail.com>
2022-08-20 13:21:30 +0200

    feat: create pipeline for data preparation

---
+++ src/unhcr_hatespeech/pipelines/data_preparation/pipeline.py
@@ -0,0 +1,30 @@
+"""
+This is a boilerplate pipeline 'data_processing'
+generated using Kedro 0.18.1
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import (
+    split_stratified_into_train_val_test,
+    oversample,
+)
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=split_stratified_into_train_val_test,
+                inputs=["model_input_combined", ["label"]],
+                outputs=["training_set", "validation_set", "test_set"],
+                name="Split Combined Input Dataset into Train, Val, and Test",
+            ),
+            node(
+                func=oversample,
+                inputs="training_set",
+                outputs="os_training_set",
+                name="Oversampling the Training Set",
+            )
+        ]
+    )
