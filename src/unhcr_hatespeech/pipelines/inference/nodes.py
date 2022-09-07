4e1a3b0ecc0a6fc7486b9306b7a28bb86c925a44
Henry Stoll <info.henry.stoll@gmail.com>
2022-09-07 09:55:11 +0200

    added todos

--- src/unhcr_hatespeech/pipelines/inference/nodes.py
+++ src/unhcr_hatespeech/pipelines/inference/nodes.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from transformers import AutoModelForSequenceClassification, pipeline, AutoTokenizer
 
-
-def download_tokenizer(tokenizer_identifier: str = "unhcr/hatespeech-detection"):
+# TODO: add to config
+def _download_tokenizer(tokenizer_identifier: str = "unhcr/hatespeech-detection"):
     return AutoTokenizer.from_pretrained(tokenizer_identifier)
 
-
-def download_classifier(classifier_identifier: str = "unhcr/hatespeech-detection"):
+# TODO: add to config
+def _download_classifier(classifier_identifier: str = "unhcr/hatespeech-detection"):
     id2label = {
         0: "Normal",
         1: "Offensive",
@@ -14,21 +14,23 @@ def download_classifier(classifier_identifier: str = "unhcr/hatespeech-detection
     }
 
     label2id = {id2label[i]: i for i in id2label}
-
+    
+    # TODO: add num_labels to config
     return AutoModelForSequenceClassification.from_pretrained(
         classifier_identifier, num_labels=3, id2label=id2label, label2id=label2id
     )
 
 
-def run_inference(data: pd.DataFrame, text_col: str = "text") -> pd.DataFrame:
-    tokenizer = download_classifier()
-    classifier = download_classifier()
+def run_inference(data: pd.DataFrame, text_col: str = "text", local: bool = True) -> pd.DataFrame:
+    tokenizer = _download_tokenizer()
+    classifier = _download_classifier()
 
     inference_pipeline = pipeline(
         task="text-classification",
         tokenizer=tokenizer,
         model=classifier,
-        # device=0,
+        # TODO: add to config
+        device= -1 if local else 0,
         return_all_scores=True,
         max_length=128,
         padding=True,
