e4ca33fa16751d5ca01d5e436af7d4d6ebfe49a9
Henry Stoll <info.henry.stoll@gmail.com>
2022-09-08 22:06:23 +0200

    changed reST docu

--- src/unhcr_hatespeech/pipelines/inference/__init__.py
+++ src/unhcr_hatespeech/pipelines/inference/__init__.py
@@ -1,15 +1,18 @@
 """
-Overview:
+Overview
+========
     Downloads a Huggingface tokenizer and transformer model from https://huggingface.co/unhcr/hatespeech-detection,
     which are used to calculate the predictions in a Huggingface transformer pipeline.
 
-Pipeline inputs:
+Pipeline inputs
+===============
     All the input datasets need to be represented as Parquest datasets.
 
     The following datasets are used as input:
         unhcr_data, hatecheck_data, test_set (from the train, val, test split of the combined_dataset)
 
-Pipeline outputs:
+Pipeline outputs
+================
     A Parquet dataset for each of the input datasets with the predictions.
 """
 
